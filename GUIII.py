bb3763b799276e21cfe8dc258c4f3e971d08522f
louis800530 <louis@huangguanzhendeMacBook-Pro.local>
2016-06-07 02:00:22 +0800

    update

--- GUIII.py
+++ GUIII.py
@@ -38,6 +38,26 @@ if __name__ == '__main__':
     
     im = Image.open("dog.jpg")
     im.show()
+    # multiply each pixel by 1.2
+    #out = im.point(lambda i: i * 1.8)
+    # split the image into individual bands
+    source = im.split()
+    
+    R, G, B = 0, 1, 2
+
+    # select regions where red is less than 100
+    mask = source[R].point(lambda i: i < 100 and 255)
+
+    # process the green band
+    out = source[G].point(lambda i: i * 0.7)
+
+    # paste the processed band back, but only where red was < 100
+    source[G].paste(out, None, mask)
+
+    # build a new multiband image
+    im = Image.merge(im.mode, source)
+    im.show()
+    #out.show()
     root= Tk()
     apX=RRR(m=root)
     apX.mainloop()
